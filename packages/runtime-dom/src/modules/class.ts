7c07ad4bd6dd55536bcb092ee022d8badf1461df
zhoulixiang <18366276315@163.com>
2024-03-07 04:47:23 +0000

    feat: test e2e

--- packages/runtime-dom/src/modules/class.ts
+++ packages/runtime-dom/src/modules/class.ts
@@ -2,6 +2,7 @@ import { type ElementWithTransition, vtcKey } from '../components/Transition'
 
 // compiler should normalize class + :class bindings on the same element
 // into a single binding ['staticClass', dynamic]
+
 export function patchClass(el: Element, value: string | null, isSVG: boolean) {
   // directly setting className should be faster than setAttribute in theory
   // if this is an element during a transition, take the temporary transition
